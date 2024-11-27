a0dacc68538568bc68850f975a89da87cd53641c
Hieroglyfic <richard.m.mauricio@gmail.com>
2024-11-26 22:58:22 -0500

    Updated Sensor (RM)
    Updated Sensor (RM)

--- Connecting_to_firestore.py
+++ Connecting_to_firestore.py
@@ -4,7 +4,7 @@ from firebase_admin import (credentials, firestore)
 def firstconnect_to_firestore():
     # Initialize Firestore with your service account key
     cred = credentials.Certificate(
-        r"C:\Users\lc201\Documents\Python Project\Sensor-Firestore\save-a-slot-firebase-adminsdk-xeee6-a6a543c43c.json")
+        r"C:\Users\richa\OneDrive\Documents\GitHub\Sensor-Firestore1\save-a-slot-firebase-adminsdk-xeee6-f9ae946a6c.json")
     firebase_admin.initialize_app(cred)
 
     # Now you can interact with your Firestore database
